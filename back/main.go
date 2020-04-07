5da44bc03d928d26914b2270bbb90b03cb56c14b
gordeev <gordeev.and.and@gmail.com>
2020-04-08 01:14:29 +0300

    added products route

--- back/main.go
+++ back/main.go
@@ -11,7 +11,8 @@ import (
 func main () {
 	log.Printf("server is running.")
 
-	api.AddRoute("/store", store.Index)
+	api.AddRoute("/store/", store.Index)
+	api.AddRoute("/store/products/", store.ProductList)
 	api.Run()
 }
 
--- back/store/controllers.go
+++ back/store/controllers.go
@@ -2,12 +2,30 @@ package store
 
 import (
   "doggo/api"
+  "strconv"
 )
 
 
 func Index(c api.Context) (interface{}, int) {
 	var data = make(map[string]string)
-	data["store"] = "!"
+	data["store"] = "0"
 	return data, 200
 }
 
+
+type Product struct {
+	id int
+	name string
+}
+
+
+func ProductList(c api.Context) (interface{}, int) {
+	var data [1]interface{}
+	product := Product{id: 1, name: "name"}
+	productData := make(map[string]string)
+	productData["name"] = product.name
+	productData["id"] = strconv.Itoa(product.id)
+	data[0] = productData
+	return data, 200
+
+}
